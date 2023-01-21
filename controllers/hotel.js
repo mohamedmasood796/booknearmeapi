298153a1f0dbdc32881c937c13a6559670232e7a
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-01-21 12:16:35 +0530

    added login signup page

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -13,6 +13,7 @@ export const createHotel = async (req, res, next) => {
 
 
 export const updateHotel = async (req, res, next) => {
+    
 
     try {
         const updatedHotel = await Hotel.findByIdAndUpdate(
@@ -39,7 +40,7 @@ export const deleteHotel = async (req, res, next) => {
 
 //get 
 export const getHotel = async (req, res, next) => {
-
+    console.log(req.params.id)
     try {
         const hotel = await Hotel.findById(req.params.id)
         res.status(200).json(hotel)
@@ -50,8 +51,9 @@ export const getHotel = async (req, res, next) => {
 
 //get all hotels
 export const getHotels = async (req, res, next) => {
+    const {min,max,...others}=req.query
     try {
-        const hotels = await Hotel.find()
+        const hotels = await Hotel.find({...others,cheapestPrice:{$gt:min ||  1,$lt:max ||999}}).limit(req.query.limit)
         res.status(200).json(hotels)
     } catch (err) {
         next(err)
@@ -67,6 +69,29 @@ export const countByCity = async (req, res, next) => {
         }))
         console.log(list)
         res.status(200).json(list)
+    } catch (err) {
+        next(err)
+    }
+}
+export const countByType = async (req, res, next) => {
+    try {
+        const hotelCount= await Hotel.countDocuments({type:"hotel"})
+        const apartmentCount= await Hotel.countDocuments({type:"apartment"})
+        const resortCount= await Hotel.countDocuments({type:"resort"})
+        const villaCount= await Hotel.countDocuments({type:"villa"})
+        const cabinCount= await Hotel.countDocuments({type:"cabin"})
+        
+        // const list= await Promise.all(cities.map(city=>{
+        //     return Hotel.countDocuments({city:city}) 
+        // }))
+        
+        res.status(200).json([
+            {type:'hotel',count:hotelCount},
+            {type:'apartments',count:apartmentCount},
+            {type:'resorts',count:resortCount},
+            {type:'villas',count:villaCount},
+            {type:'cabins',count:cabinCount},
+        ])
     } catch (err) {
         next(err)
     }
