57168f3119506d8aa8ae4b913e553cccf3353c93
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-02-24 13:56:35 +0530

    to start host

--- controllers/auth.js
+++ controllers/auth.js
@@ -25,7 +25,7 @@ export const register = async (req, res, next) => {
                 password: hash,
             }).save()
             console.log(newUser,"new user details");
-            const url=`http://localhost:3000/verify?id=${newUser._id}`
+            const url=`https://booknearme.hamrix.store/verify?id=${newUser._id}`
             console.log(req.body.email);
            await sendEmail(req.body.email, "click to verify your account", url)
 
