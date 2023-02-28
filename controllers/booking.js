f4635262d78a7fc4861727810c2be9014f569ab8
mohamedmasood796 <mohamedmasood796@gmail.com>
2023-02-28 20:59:01 +0530

    Browse by property type in home page

--- controllers/booking.js
+++ controllers/booking.js
@@ -4,57 +4,68 @@ import jwt from "jsonwebtoken";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const router = express.Router();
-console.log(stripe,"masooo alab")
+console.log(stripe, "masooo alab")
 
 import Booking from "../models/Booking.js"
 
 export const booking = async (req, res, next) => {
 
   console.log('req.body:', req.body); // Log the request body
-  const {_id,...product}=req.body;
+  const { _id, ...product } = req.body;
   const tokenData = {
-    _id,...product
+    _id, ...product
   }
 
   // create jwt token
   const token = jwt.sign(tokenData, process.env.JWT)
 
-    const unitAmount = parseInt(product.newOrder.price * 100);
-    console.log('unitAmount:', unitAmount); // Log the unit amount
-    console.log('Amount:', product); // Log the unit amount
-    const session = await stripe.checkout.sessions.create({ 
-        payment_method_types: ["card"], 
-        line_items: [ 
-          { 
-            price_data: { 
-              currency: "inr", 
-              product_data: { 
-                name: product.newOrder.name, 
-              }, 
-              unit_amount: unitAmount, 
-            }, 
-            quantity: parseInt(product.newOrder.numberOfNights), 
-          }, 
-        ], 
-        mode: "payment", 
-        success_url: (`${process.env.CLIENT_URL}/success?token=${token}`), 
-        cancel_url:`${process.env.CLIENT_URL}/cancel`, 
-      }); 
-    // res.json({ id: session.id });
-    res.send({ url: session.url });
+  const unitAmount = parseInt(product.newOrder.price * 100);
+  console.log('unitAmount:', unitAmount); // Log the unit amount
+  console.log('Amount:', product); // Log the unit amount
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [
+      {
+        price_data: {
+          currency: "inr",
+          product_data: {
+            name: product.newOrder.name,
+          },
+          unit_amount: unitAmount,
+        },
+        quantity: parseInt(product.newOrder.numberOfNights),
+      },
+    ],
+    mode: "payment",
+    success_url: (`${process.env.CLIENT_URL}/success?token=${token}`),
+    cancel_url: `${process.env.CLIENT_URL}/cancel`,
+  });
+  // res.json({ id: session.id });
+  res.send({ url: session.url });
 };
 
 
 export const verify = async (req, res, next) => {
-  const {newOrder,userId}=req.body
-console.log(userId,2855);
-  console.log(newOrder,"data token is here")
+  const { newOrder, userId } = req.body
+  console.log(userId, 2855);
+  console.log(newOrder, "data token is here")
   const verify = await jwt.verify(newOrder, process.env.JWT)
   console.log(verify.newOrder);
-  let insideobj=verify.newOrder
-  const newarr={...insideobj,userId}
-  const {_id,...others}=newarr
-  console.log(others,"this is token form jw")
+  let insideobj = verify.newOrder
+  const newarr = { ...insideobj, userId }
+  const { _id, ...others } = newarr
+  console.log(others, "this is token form jw")
   const newHotel = new Booking(others);
   const savedOrder = await newHotel.save()
+}
+
+export const bookings=async(req,res,next)=>{
+  console.log("hello just checking ", req.body)
+  try {
+      const bookingsdata = await Booking.find(req.body)
+      console.log(bookingsdata);
+      res.status(200).json(bookingsdata)
+  } catch (err) {
+      next(err)
+  }
 }
--- routers/booking.js
+++ routers/booking.js
@@ -1,12 +1,13 @@
 
 
 import express from "express";
-import { booking, verify } from "../controllers/booking.js";
+import { booking, bookings, verify } from "../controllers/booking.js";
 const router=express.Router()
 import authMiddleware from "../utils/authJwtMiddleware.js";
 
 router.post("/booking",booking)
 router.post("/verify",authMiddleware,verify)
+router.get("/bookings",authMiddleware,bookings)
 
 
 
